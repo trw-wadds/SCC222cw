b0b93079955193117b7ed79eaf13b5ff9d4923b9
Tom Waddington <tomwaddington2005@gmail.com>
2025-03-21 19:32:29 +0000

    start SVM

--- Main.py
+++ Main.py
@@ -7,14 +7,6 @@ import time
 from KNNClassifier import KNNClassifier
 from NaiveBayes import NaiveBayes
 
-student_id = 38995239
-random.seed(student_id)
-header = ["ID","RI","Na","Mg","Al","Si","K","Ca","Ba","Fe","Type"]
-
-df = pd.read_csv("glass.data",header=None,names=header)
-df = df.drop(df.columns[0], axis=1)
-# print(df)
-
 def train_test_split(x, y, test_size=0.2, seed=student_id):
     if len(x) != len(y):
         raise ValueError("There cannot be a different number of features and labels")
@@ -32,79 +24,89 @@ def train_test_split(x, y, test_size=0.2, seed=student_id):
 
     return np.array(x_train), np.array(x_test), np.array(y_train), np.array(y_test)
 
-### train test splot
-data = df[header[1:-1]].values
-labels = df['Type'].values
-x_train, x_test, y_train, y_test = train_test_split(data, labels)
+if __name__ == "__main__":
+      student_id = 38995239
+      random.seed(student_id)
+      header = ["ID","RI","Na","Mg","Al","Si","K","Ca","Ba","Fe","Type"]
 
-### KNN
-# train
-knn = KNNClassifier()
-train_ts1 = time.time()
-knn.fit(x_train, y_train)
-train_ts2 = time.time()
+      df = pd.read_csv("glass.data",header=None,names=header)
+      df = df.drop(df.columns[0], axis=1)
+      # print(df)
 
-# test
-test_ts1 = time.time()
-predicted_classes = knn.predict(x_test)
-test_ts2 = time.time()
 
-# evaluation
-accuracy = knn.accuracy_score(x_test, y_test)
-print(F"Accuracy: {accuracy * 100:.2f}%")
-print(F"Time taken to train: {train_ts2 - train_ts1} seconds"
-      F"\nTime taken to test: {test_ts2 - test_ts1} seconds")
+      ### train test splot
+      data = df[header[1:-1]].values
+      labels = df['Type'].values
+      x_train, x_test, y_train, y_test = train_test_split(data, labels)
 
-### Naïve Bayes
-# train
-nb = NaiveBayes()
-train_ts1 = time.time()
-nb.fit(x_train, y_train)
-train_ts2 = time.time()
+      ### KNN
+      # train
+      knn = KNNClassifier()
+      train_ts1 = time.time()
+      knn.fit(x_train, y_train)
+      train_ts2 = time.time()
 
-# test
-test_ts1 = time.time()
-predicted_classes = knn.predict(x_test)
-test_ts2 = time.time()
+      # test
+      test_ts1 = time.time()
+      predicted_classes = knn.predict(x_test)
+      test_ts2 = time.time()
 
-# evaluation
-accuracy = nb.accuracy_score(x_test, y_test)
-print(F"Accuracy: {accuracy * 100:.2f}%")
-print(F"Time taken to train: {train_ts2 - train_ts1} seconds"
-      F"\nTime taken to test: {test_ts2 - test_ts1} seconds")
+      # evaluation
+      accuracy = knn.accuracy_score(x_test, y_test)
+      print(F"Accuracy: {accuracy * 100:.2f}%")
+      print(F"Time taken to train: {train_ts2 - train_ts1} seconds"
+            F"\nTime taken to test: {test_ts2 - test_ts1} seconds")
 
-### Decision Tree
-# train
+      ### Naïve Bayes
+      # train
+      nb = NaiveBayes()
+      train_ts1 = time.time()
+      nb.fit(x_train, y_train)
+      train_ts2 = time.time()
 
-train_ts1 = time.time()
+      # test
+      test_ts1 = time.time()
+      predicted_classes = knn.predict(x_test)
+      test_ts2 = time.time()
 
-train_ts2 = time.time()
+      # evaluation
+      accuracy = nb.accuracy_score(x_test, y_test)
+      print(F"Accuracy: {accuracy * 100:.2f}%")
+      print(F"Time taken to train: {train_ts2 - train_ts1} seconds"
+            F"\nTime taken to test: {test_ts2 - test_ts1} seconds")
 
-# test
-test_ts1 = time.time()
+      ### Decision Tree
+      # train
 
-test_ts2 = time.time()
+      train_ts1 = time.time()
 
-# evaluation
-accuracy = 0
-print(F"Accuracy: {accuracy * 100:.2f}%")
-print(F"Time taken to train: {train_ts2 - train_ts1} seconds"
-      F"\nTime taken to test: {test_ts2 - test_ts1} seconds")
+      train_ts2 = time.time()
 
-### Support Vector Machine
-# train
+      # test
+      test_ts1 = time.time()
 
-train_ts1 = time.time()
+      test_ts2 = time.time()
 
-train_ts2 = time.time()
+      # evaluation
+      accuracy = 0
+      print(F"Accuracy: {accuracy * 100:.2f}%")
+      print(F"Time taken to train: {train_ts2 - train_ts1} seconds"
+            F"\nTime taken to test: {test_ts2 - test_ts1} seconds")
 
-# test
-test_ts1 = time.time()
+      ### Support Vector Machine
+      # train
 
-test_ts2 = time.time()
+      train_ts1 = time.time()
 
-# evaluation
-accuracy = 0
-print(F"Accuracy: {accuracy * 100:.2f}%")
-print(F"Time taken to train: {train_ts2 - train_ts1} seconds"
-      F"\nTime taken to test: {test_ts2 - test_ts1} seconds")
+      train_ts2 = time.time()
+
+      # test
+      test_ts1 = time.time()
+
+      test_ts2 = time.time()
+
+      # evaluation
+      accuracy = 0
+      print(F"Accuracy: {accuracy * 100:.2f}%")
+      print(F"Time taken to train: {train_ts2 - train_ts1} seconds"
+            F"\nTime taken to test: {test_ts2 - test_ts1} seconds")
--- SVM.py
+++ SVM.py
@@ -1,14 +1,55 @@
-import numpy
+import numpy as np
 
 class SVM():
-    def __init__():
+    def __init__(self, learning_rate=1e-3, lambda_param=1e-2, n_iterations=100):
+        self.n_iterations = n_iterations
+        self.lambda_param = lambda_param
+        
+        self.hyperplanes = []
+
+    def fit(self, X, y):
+        # create list of all unique class pairings
+        n_samples, n_features = X.shape
+        self.unique_classes = np.unique(y)
+        self.class_pairings = [
+            (c1, c2)
+            for i, c1 in enumerate(self.unique_classes)
+            for c2 in self.unique_classes[i+1:]
+        ]
+        # iterate through, creating a hyperplane for each
+        for c1, c2 in self.class_pairings:
+            hyperPlane = HyperPlane()
+            hyperPlane.train(X, y, n_samples, n_features)
+            self.hyperplanes.append(hyperPlane)
         pass
 
-    #ASSUME LINEAR SEPERABILITY
-
-    def fit(self, X):
-        pass
+    def predict(self, X):
+        for h in self.hyperplanes:
+            h._predict(X)
 
     def accuracy_score(self, x_test, y_test):
         predictions = self.predict(x_test)
         return np.mean(predictions == y_test)
+    
+class HyperPlane():
+    def __init__(self, learning_rate=1e-3, lambda_param=1e-2, n_iterations=100):
+        self.learning_rate = learning_rate
+        self.lambda_param = lambda_param
+        self.n_iterations = n_iterations
+        self.bias = None
+        self.weights = None
+
+    def train(self, X, y, n_samples, n_features):
+        self.weights = np.zeros(n_samples)
+        y_decision = np.where(y <= 0, -1, 1)
+
+        for i in range(self.n_iterations):
+            for id, x in enumerate(X):
+                if (y_decision[id] * np.dot(x, self.weight) - self.bias) >= 1:
+                    self.weights -= self.learning_rate * (2 * self.lambda_param * self.weights)
+                else:
+                    self.weights -= self.learning_rate * (2 * self.lambda_param * self.weights - np.dot(x, y[id]))
+                    self.bias -= self.learning_rate * y[id]
+
+    def predict(self, X):
+        return np.sign(np.dot(X, self.weights) - self.bias)
